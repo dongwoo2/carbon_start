5c7267aa5a4dc101183a7c8a8a172a63b2d3d296
dongwoo2 <ieiedong@naver.com>
2024-04-26 23:30:04 +0900

    20240426 데이터베이스 mysql로 교체완료

---
+++ carbonindustry/mysite/mysettings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'carbon',
+        'USER':'root',
+        'PASSWORD':'4106',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-ae2cd538wzz7sharaql^o*0$n32zrwj+zh00nen-vh6+w*1*g@'
--- carbonindustry/mysite/settings.py
+++ carbonindustry/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from . import mysettings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ae2cd538wzz7sharaql^o*0$n32zrwj+zh00nen-vh6+w*1*g@'
+#SECRET_KEY = 'django-insecure-ae2cd538wzz7sharaql^o*0$n32zrwj+zh00nen-vh6+w*1*g@'
 
+SECRET_KEY = mysettings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -73,13 +74,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = mysettings.DATABASES
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
