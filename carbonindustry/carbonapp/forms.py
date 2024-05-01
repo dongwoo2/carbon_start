bbf4f41abf55178081e7900f1910642f64bd5b27
dongwoo2 <ieiedong@naver.com>
2024-05-01 17:27:54 +0900

    20240501 name = edit 세부 목록 보기에서 수정 생성

--- carbonindustry/carbonapp/forms.py
+++ carbonindustry/carbonapp/forms.py
@@ -6,3 +6,17 @@ class BoardForm(forms.ModelForm):  # 유효성 검증은 아직 안한 상태
     class Meta:
         model = Board  # 데이터는 보드를 쓰겠다
         fields = ["title", "writer", "content"]  # 필드는 이거 3개의 형태를 받겠다.
+
+
+class BoardUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Board
+        fields = ["title", "writer", "content"]
+
+    writer = forms.CharField(disabled=True)  # writer 인풋폼은 수정 불가하게 이것도 재정의 한 것
+
+
+class BoardSearchForm(forms.Form):
+    keyword = forms.CharField(
+        label="키워드:", required=True
+    )  # 인풋폼에 문자열 받을 수 있게 charField 설정
--- carbonindustry/carbonapp/views.py
+++ carbonindustry/carbonapp/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 from .models import Board
+from .forms import BoardUpdateForm
+from .forms import BoardSearchForm
+from django.shortcuts import reverse
 
 # Create your views here.
 
@@ -33,3 +36,18 @@ class BoardDetail(generic.DetailView):
         item = super().get_object()
         item.increment_read_count()
         return item
+
+
+class BoardUpdate(generic.UpdateView):
+    model = Board
+    form_class = BoardUpdateForm
+
+    def get_success_url(self):
+        return reverse("carbonfarm:detail", args=(self.object.id,))
+
+    def get_context_data(
+        self, **kwargs: any
+    ) -> dict[str, any]:  # > dict[str, any] 는 반환자료형이 무엇인지 명시해주는 것 글등록에 있는 함수처럼 없어도 무관함
+        context = super().get_context_data(**kwargs)
+        context["button_label"] = "수정"  # 파라미터를 추가해주는 것
+        return context
